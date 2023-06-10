0ae564b0713a3bd7cdcf3ccc38b207cdbaffeee1
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-10 19:37:06 +0300

    solved array modifier

---
+++ Fundamentals/02_programming_fundamentals_mid_exam/array_modifier.py
@@ -0,0 +1,26 @@
+array = [int(num) for num in input().split()]
+
+command = input()
+
+while command != "end":
+    command = command.split()
+    command_word = command[0]
+
+    if command_word == "swap":
+        index1 = int(command[1])
+        index2 = int(command[2])
+        array[index1], array[index2] = array[index2], array[index1]
+
+    elif command_word == "multiply":
+        index1 = int(command[1])
+        index2 = int(command[2])
+        result = array[index1] * array[index2]
+        array[index1] = result
+
+    elif command_word == "decrease":
+        for i in range(len(array)):
+            array[i] -= 1
+
+    command = input()
+
+print(*array, sep=", ")
