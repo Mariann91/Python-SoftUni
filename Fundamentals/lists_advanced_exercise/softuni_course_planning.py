a75543ed15fd62367c4d7d3eb6cabfa4886dfa10
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-11 21:24:32 +0300

    solved softuni course planning exercise

--- Fundamentals/lists_advanced_exercise/softuni_course_planning.py
+++ Fundamentals/lists_advanced_exercise/softuni_course_planning.py
@@ -1,52 +1,71 @@
+def check_lesson(input_list, name):
+    if name in input_list:
+        return True
+    return False
+
+
 lessons = input().split(", ")
 
-lesson = input()
+command = input()
 
-while lesson != "course start":
-    lesson = lesson.split(":")
-    command = lesson[0]
+while command != "course start":
+    command = command.split(":")
+    command_word = command[0]
 
-    if command == "Add":
-        lesson_name = lesson[1]
-        if lesson_name not in lessons:
-            lessons.append(lesson_name)
+    if command_word == "Add":
+        lesson = command[1]
+        if not check_lesson(lessons, lesson):
+            lessons.append(lesson)
 
-    elif command == "Insert":
-        lesson_name = lesson[1]
-        lesson_index = int(lesson[2])
-        if lesson_name not in lessons:
-            lessons.insert(lesson_index, lesson_name)
+    elif command_word == "Insert":
+        lesson = command[1]
+        insert_index = int(command[2])
+        if not check_lesson(lessons, lesson):
+            lessons.insert(insert_index, lesson)
 
-    elif command == "Remove":
-        lesson_name = lesson[1]
-        if lesson_name in lessons:
-            lessons.remove(lesson_name)
+    elif command_word == "Remove":
+        lesson = command[1]
 
-    elif command == "Swap":
-        lesson1_name = lesson[1]
-        lesson2_name = lesson[2]
-        lesson1_name_index = lessons.index(lesson1_name)
-        lesson2_name_index = lessons.index(lesson2_name)
-        if lesson1_name in lessons and lesson2_name in lessons:
-            swap_help = lessons[lesson1_name_index]
-            lessons[lesson1_name_index] = lessons[lesson2_name_index]
-            lessons[lesson2_name_index] = swap_help
+        if check_lesson(lessons, lesson):
+            lessons.remove(lesson)
+            exercise_name = f"{lesson}-Exercise"
+            if exercise_name in lessons:
+                lessons.remove(exercise_name)
 
-            
+    elif command_word == "Swap":
+        first_lesson = command[1]
+        second_lesson = command[2]
+        if check_lesson(lessons, first_lesson) and check_lesson(lessons, second_lesson):
+            first_lesson_index = lessons.index(first_lesson)
+            second_lesson_index = lessons.index(second_lesson)
+            lessons[first_lesson_index], lessons[second_lesson_index] = \
+                lessons[second_lesson_index], lessons[first_lesson_index]
 
-    else:
-        lesson_name = lesson[1]
-        if lesson_name in lessons:
-            exercise_index = lessons.index(lesson_name) + 1
-            lessons.insert(exercise_index, f"{lesson_name}-Exercise")
+            first_exercise_name = f"{first_lesson}-Exercise"
+            second_exercise_name = f"{second_lesson}-Exercise"
+
+            if first_exercise_name in lessons and lessons.index(first_exercise_name) != lessons.index(first_lesson) + 1:
+                lessons.remove(first_exercise_name)
+                lessons.insert(lessons.index(first_lesson) + 1, first_exercise_name)
+
+            if second_exercise_name in lessons and lessons.index(second_exercise_name)\
+                    != lessons.index(second_lesson) + 1:
+                lessons.remove(second_exercise_name)
+                lessons.insert(lessons.index(second_lesson) + 1, second_exercise_name)
+
+    elif command_word == "Exercise":
+        lesson = command[1]
+        exercise_name = f"{lesson}-Exercise"
+
+        if check_lesson(lessons, lesson):
+            lesson_index = lessons.index(lesson)
+            lessons.insert(lesson_index + 1, exercise_name)
+
         else:
-            lessons.append(lesson_name)
-            lessons.append(f"{lesson_name}-Exercise")
+            lessons.append(lesson)
+            lessons.append(exercise_name)
 
-    print(lessons)
-    lesson = input()
+    command = input()
 
-
-#Data Types, Objects, Lists
-
-#Arrays, Lists, Methods
+for position in range(1, len(lessons) + 1):
+    print(f"{position}.{lessons[position - 1]}")
