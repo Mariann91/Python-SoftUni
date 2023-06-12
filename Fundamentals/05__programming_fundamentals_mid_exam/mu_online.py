81e584caec0e6c150316345964e3ef05963188ae
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-12 16:29:26 +0300

    Update mu_online.py
    Updated solution for mu online exercise

--- Fundamentals/05__programming_fundamentals_mid_exam/mu_online.py
+++ Fundamentals/05__programming_fundamentals_mid_exam/mu_online.py
@@ -1,38 +1,37 @@
-rooms = input().split("|")
 MAX_HEALTH = 100
 
-health = 100
-bitcoins = 0
-room_number = 0
+rooms = input().split("|")
 
+health, bitcoins = 100, 0
+room_count = 0
 for room in rooms:
-  
-  room_number += 1
+  room_count += 1
   room = room.split()
-  command = room[0]
-  number = int(room[1])
 
-  if command == "potion":
-    
-    if health + number <= MAX_HEALTH:
-      health += number
-    else:
-      number = MAX_HEALTH - health
-      health += number
-    print(f"You healed for {number} hp.")
+  command_word = room[0]
+  value = int(room[1])
+
+  if command_word == "potion":
+    if health + value > 100:
+      value = MAX_HEALTH - health
+
+    health += value
+    print(f"You healed for {value} hp.")
     print(f"Current health: {health} hp.")
 
-  elif command == "chest":
-    bitcoins += number
-    print(f"You found {number} bitcoins.")
+  elif command_word == "chest":
+    bitcoins += value
+    print(f"You found {value} bitcoins.")
 
   else:
-    health -= number
-    if health > 0:
-      print(f"You slayed {command}.")
-    else:
-      print(f"You died! Killed by {command}.")
-      print(f"Best room: {room_number}")
+    health -= value
+    if health <= 0:
+      print(f"You died! Killed by {command_word}." )
+      print(f"Best room: {room_count}")
       break
+    else:
+      print(f"You slayed {command_word}.")
 else:
-  print(f"You've made it!\nBitcoins: {bitcoins}\nHealth: {health}")
+  print("You've made it!")
+  print(f"Bitcoins: {bitcoins}")
+  print(f"Health: {health}")
