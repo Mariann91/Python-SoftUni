0abfdc500c35cca19dcf9c5d6ae76537f0be0af8
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-12 14:32:51 +0300

    Update man_war.py
    Updated man war exercise

--- Fundamentals/06_programming_fundamentals_mid_exam/man_war.py
+++ Fundamentals/06_programming_fundamentals_mid_exam/man_war.py
@@ -1,68 +1,65 @@
-def check_index(ship, index):
-    if 0 <= index < len(ship):
-        return True
-    else:
-        return False
+def check_index(index, input_list):
+  if 0 <= index < len(input_list):
+    return True
+  return False
+  
 
-
-pirate_ship_status = [int(num) for num in input().split(">")]
-war_ship_status = [int(num) for num in input().split(">")]
-sec_max_health = int(input())
+pirate_sections = [int(num) for num in input().split(">")]
+warship_sections = [int(num) for num in input().split(">")]
+section_max_health = int(input())
 
 command = input()
 
-break_flag = False
+draw_flag = True
 while command != "Retire":
-    command = command.split()
-    command_word = command[0]
+  command = command.split()
+  command_word = command[0]
 
-    if command_word == "Fire":
-        warship_section = int(command[1])
-        damage_dealt = int(command[2])
+  if command_word == "Fire":
+    attacked_warship_section = int(command[1])
+    damage = int(command[2])
+    
+    if check_index(attacked_warship_section, warship_sections):
+      warship_section = int(command[1])
+      damage = int(command[2])
+      warship_sections[warship_section] -= damage
+    
+      if warship_sections[warship_section] <= 0:
+        print("You won! The enemy ship has sunken.")
+        draw_flag = False
+        break
 
-        if check_index(war_ship_status, warship_section):
-            war_ship_status[warship_section] -= damage_dealt
-            if war_ship_status[warship_section] <= 0:
-                print("You won! The enemy ship has sunken.")
-                break
+  elif command_word == "Defend":
+    start_point = int(command[1])
+    end_point = int(command[2])
+    damage = int(command[3])
 
-    elif command_word == "Defend":
-        start_index = int(command[1])
-        end_index = int(command[2])
-        damage_dealt = int(command[3])
-        if check_index(pirate_ship_status, start_index) and check_index(pirate_ship_status, end_index):
-            for i in range(start_index, end_index + 1):
-                pirate_ship_status[i] -= damage_dealt
-                if pirate_ship_status[i] <= 0:
-                    print("You lost! The pirate ship has sunken.")
-                    break_flag = True
-                    break
-            if break_flag:
-                break
+    if check_index(start_point, pirate_sections) and check_index(end_point, pirate_sections):
+      for i in range(start_point, end_point + 1):
+        pirate_sections[i] -= damage
+        if pirate_sections[i] <= 0:
+          print("You lost! The pirate ship has sunken.")
+          draw_flag = False
+          break
+  elif command_word == "Repair":
+    repair_index = int(command[1])
+    health = int(command[2])
 
-    elif command_word == "Repair":
-        section = int(command[1])
-        gained_health = int(command[2])
+    if check_index(repair_index, pirate_sections):
+      if pirate_sections[repair_index] + health > section_max_health:
+        health = section_max_health - pirate_sections[repair_index]
+      pirate_sections[repair_index] += health
 
-        if check_index(pirate_ship_status, section):
-            if pirate_ship_status[section] + gained_health <= sec_max_health:
-                pirate_ship_status[section] += gained_health
-            else:
-                gained_health = sec_max_health - pirate_ship_status[section]
-                pirate_ship_status[section] += gained_health
+  elif command_word == "Status":
+    bellow_max_counter = 0
+    twenty_percent = section_max_health * 0.20
+    for i in range(len(pirate_sections)):
+      if pirate_sections[i] < twenty_percent:
+        bellow_max_counter += 1
+    print(f"{bellow_max_counter} sections need repair.")
+    
+  command = input()
 
-    elif command_word == "Status":
-        repair_limit = sec_max_health * 0.20
-
-        section_counter = 0
-        for i in range(len(pirate_ship_status)):
-            if pirate_ship_status[i] < repair_limit:
-                section_counter += 1
-        print(f"{section_counter} sections need repair.")
-
-    command = input()
-
-else:
-    print(f"Pirate ship status: {sum(pirate_ship_status)}")
-    print(f"Warship status: {sum(war_ship_status)}")
-
+if draw_flag:
+  print(f"Pirate ship status: {sum(pirate_sections)}")
+  print(f"Warship status: {sum(warship_sections)}")
