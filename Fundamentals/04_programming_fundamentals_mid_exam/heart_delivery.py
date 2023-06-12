a2b68f05370ff863d56ebe3d4dacec4c941677e8
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-12 20:45:06 +0300

    Updated solution for heart delivery exercise

--- Fundamentals/04_programming_fundamentals_mid_exam/heart_delivery.py
+++ Fundamentals/04_programming_fundamentals_mid_exam/heart_delivery.py
@@ -1,57 +1,30 @@
-def check_index(index, input_list):
-  if 0 <= index < len(input_list):
-    return True
-  return False
-
-
-def check_zero_hearts(index, input_list):
-  if input_list[index] == 0:
-    return True
-  return False
-
-
-def check_result(input_list):
-  if input_list.count(0) == len(input_list):
-    return True
-  return False
-
-
-def count(input_list):
-  not_zeros = [num for num in input_list if num != 0]
-  return len(not_zeros)
-
 hearts = [int(num) for num in input().split("@")]
 
-jump = input()
+jump_len = input()
 
 current_index = 0
-while jump != "Love!":
-  jump = jump.split()
-  jump_len = int(jump[1])
-  current_index += jump_len
+while jump_len != "Love!":
+    jump_len = jump_len.split()
+    index = int(jump_len[1])
 
-  if check_index(current_index, hearts):
-    if check_zero_hearts(current_index, hearts):
-      print(f"Place {current_index} already had Valentine's day.")
-    else:
-      hearts[current_index] -= 2
-      if check_zero_hearts(current_index, hearts):
-        print(f"Place {current_index} has Valentine's day.")
-  else:
-    current_index = 0
-    if check_zero_hearts(current_index, hearts):
-      print(f"Place {current_index} already had Valentine's day.")
-    else:
-      hearts[current_index] -= 2
-      if check_zero_hearts(current_index, hearts):
-        print(f"Place {current_index} has Valentine's day.") 
+    current_index += index
 
-  jump = input()
+    if current_index >= len(hearts):
+        current_index = 0
+
+    if hearts[current_index] == 0:
+        print(f"Place {current_index} already had Valentine's day.")
+    else:
+        hearts[current_index] -= 2
+        if hearts[current_index] <= 0:
+            hearts[current_index] = 0
+            print(f"Place {current_index} has Valentine's day.")
+
+    jump_len = input()
 
 print(f"Cupid's last position was {current_index}.")
 
-if check_result(hearts):
-  print("Mission was successful.")
+if hearts.count(0) == len(hearts):
+    print("Mission was successful.")
 else:
-  print(f"Cupid has failed {count(hearts)} places.")
-  
+    print(f"Cupid has failed {len(hearts) - hearts.count(0)} places.")
