c4713e2dc1dfc885af9bdf14fb8ed2decb0ca03f
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-12 20:27:10 +0300

    Update shopping_list.py
    Updated solution for shopping list exercise

--- Fundamentals/04_programming_fundamentals_mid_exam/shopping_list.py
+++ Fundamentals/04_programming_fundamentals_mid_exam/shopping_list.py
@@ -1,33 +1,39 @@
-def does_it_exists(item, item_list):
-  if item in item_list:
-    return True
-  return False
+def check_item(item, item_list):
+    if item in item_list:
+        return True
+    return False
 
-input_list = input().split("!")
+
+product_list = input().split("!")
 
 command = input()
 
 while command != "Go Shopping!":
-  command = command.split()
-  command_word = command[0]
+    command = command.split()
+    command_word = command[0]
 
-  if command_word == "Urgent" and not does_it_exists(command[1], input_list):
-    input_list.insert(0, command[1])
+    if command_word == "Urgent":
+        item_to_add = command[1]
+        if not check_item(item_to_add, product_list):
+            product_list.insert(0, item_to_add)
 
-  elif command_word == "Unnecessary" and does_it_exists(command[1], input_list):
-    input_list.remove(command[1])
+    elif command_word == "Unnecessary":
+        item_to_remove = command[1]
+        if check_item(item_to_remove, product_list):
+            product_list.remove(item_to_remove)
+    elif command_word == "Correct":
+        old_item = command[1]
+        new_item = command[2]
+        if check_item(old_item, product_list):
+            old_item_index = product_list.index(old_item)
+            product_list[old_item_index] = new_item
 
-  elif command_word == "Correct":
-    old_item = command[1]
-    new_item = command[2]
-    if does_it_exists(old_item, input_list):
-      old_item_index = input_list.index(old_item)
-      input_list[old_item_index] = new_item
+    elif command_word == "Rearrange":
+        rearrange_item = command[1]
+        if check_item(rearrange_item, product_list):
+            rearrange_item_index = product_list.index(rearrange_item)
+            product_list.append(product_list.pop(rearrange_item_index))
 
-  elif command_word == "Rearrange" and does_it_exists(command[1], input_list):
-    input_list.remove(command[1])
-    input_list.append(command[1])
-    
-  command = input()
+    command = input()
 
-print(", ".join(input_list))
+print(", ".join(product_list))
