e7ea664195678ca7ca669d926afc7d7a0513b7fd
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-13 20:58:37 +0300

    updated solution for softuni reception exercise

--- Fundamentals/02_programming_fundamentals_mid_exam/softuni_reception.py
+++ Fundamentals/02_programming_fundamentals_mid_exam/softuni_reception.py
@@ -1,16 +1,14 @@
 from math import ceil
 
-employer1 = int(input())
-employer2 = int(input())
-employer3 = int(input())
-students_count = int(input())
+employer1, employer2, employer3 = int(input()), int(input()), int(input())
+student_count = int(input())
 
-students_per_hour = employer1 + employer2 + employer3
-hours = ceil(students_count / students_per_hour)
-hours_break = hours
+student_per_hour = employer1 + employer2 + employer3
+hours_interval = ceil(student_count / student_per_hour)
+hours_break = hours_interval
 
-for hour in range(1, hours + 1):
-    if hour % 3 == 0 and hour != hours:
+for hour in range(1, hours_interval + 1):
+    if hour % 3 == 0 and hour != hours_interval:
         hours_break += 1
 
 print(f"Time needed: {hours_break}h.")
