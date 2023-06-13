03afb3dced40426afd276d417e78a06ec5d727f2
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-13 13:54:30 +0300

    Update shoot_for_the_win.py
    Updated solution for shoot for the win

--- Fundamentals/03_programming_fundamentals_mid_exam/shoot_for_the_win.py
+++ Fundamentals/03_programming_fundamentals_mid_exam/shoot_for_the_win.py
@@ -1,40 +1,22 @@
-def check_index(index, input_list):
-  if 0 <= index < len(input_list):
-    return True 
-  return False
-
-
-def reduce_increase_value(index, input_list):
-  target_value = input_list[index]
-
-  for i in range(len(input_list)):
-    if input_list[i] > target_value and input_list[i] not in shot_targets:
-      input_list[i] -= target_value
-      
-    elif input_list[i] <= target_value and input_list[i] not in shot_targets:
-      input_list[i] += target_value
-  return input_list
-  
-
 targets = [int(num) for num in input().split()]
 
-shot_targets_count = 0
+shoot_index = input()
+shoot_targets = 0
+while shoot_index != "End":
+  shoot_index = int(shoot_index)
+  
+  if 0 <= shoot_index < len(targets):
+    current_target = targets[shoot_index]
+    targets[shoot_index] = -1
+    shoot_targets += 1
 
-shot_targets = []
-while True:
-  current_index = input()
+    for i in range(len(targets)):
+      if targets[i] > current_target and targets[i] != targets[shoot_index]:
+        targets[i] -= current_target
+      elif targets[i] <= current_target and targets[i] != targets[shoot_index]:
+        targets[i] += current_target 
 
-  if current_index == "End":
-    print(f"Shot targets: {shot_targets_count} -> ", end="")
-    print(*targets, sep=" ")
-    break
+  shoot_index = input()
 
-  current_index = int(current_index)
-
-  if check_index(current_index, targets):
-    if targets[current_index] != -1:
-      targets = reduce_increase_value(current_index, targets)
-      targets[current_index] = -1
-      shot_targets.append(targets[current_index])
-      shot_targets_count += 1
+print(f"Shot targets: {shoot_targets} -> ", *targets, sep=" ")
       
