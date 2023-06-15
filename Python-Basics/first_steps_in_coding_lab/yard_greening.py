b2dcbcf6c3875f46dbddbb860c89f6008aab5f9e
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-15 14:46:25 +0300

    Create yard_greening.py

---
+++ Python-Basics/first_steps_in_coding_lab/yard_greening.py
@@ -0,0 +1,6 @@
+greening_price = float(input()) * 7.61
+
+discount = greening_price * 0.18
+final_price = greening_price - discount
+
+print(f"The final price is: {final_price:.2f} lv.\nThe discount is: {discount:.2f} lv.")
