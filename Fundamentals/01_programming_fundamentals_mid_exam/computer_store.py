4ba7c8d2442787ed1453cab73616101cfcf8db51
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-17 09:41:48 +0300

    updated solution for computer store exercise

--- Fundamentals/01_programming_fundamentals_mid_exam/computer_store.py
+++ Fundamentals/01_programming_fundamentals_mid_exam/computer_store.py
@@ -1,24 +1,29 @@
-part_price = input()
-
 total_price = 0
-while part_price != "special" and part_price != "regular":
-    part_price = float(part_price)
 
-    if part_price < 0:
+parts_price = input()
+
+while parts_price != "special" and parts_price != "regular":
+    parts_price = float(parts_price)
+
+    if parts_price < 0:
         print("Invalid price!")
     else:
-        total_price += part_price
+        total_price += parts_price
 
-    part_price = input()
+    parts_price = input()
 
 taxes = total_price * 0.20
 price_with_taxes = total_price + taxes
 
-if part_price == "special":
-    price_with_taxes -= price_with_taxes * 0.10
+if parts_price == "special":
+    price_with_taxes *= 0.90
 
-if total_price > 0:
-    print(f"Congratulations you've just bought a new computer!\nPrice without taxes: {total_price:.2f}$\n"
-          f"Taxes: {taxes:.2f}$\n-----------\nTotal price: {price_with_taxes:.2f}$")
-else:
+
+if total_price == 0:
     print("Invalid order!")
+else:
+    print(f"Congratulations you've just bought a new computer!\n"
+          f"Price without taxes: {total_price:.2f}$\n"
+          f"Taxes: {taxes:.2f}$\n"
+          f"-----------\n"
+          f"Total price: {price_with_taxes:.2f}$")
