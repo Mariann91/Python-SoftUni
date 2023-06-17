90ed74beacc8ae10af53929b53c2b5b05965c7c5
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-17 09:54:40 +0300

    updated solution for the lift exercise

--- Fundamentals/01_programming_fundamentals_mid_exam/the_lift.py
+++ Fundamentals/01_programming_fundamentals_mid_exam/the_lift.py
@@ -1,22 +1,23 @@
+MAX_SIZE = 4
+
 people_count = int(input())
-wagon_state = [int(num) for num in input().split()]
+wagons = [int(wagon) for wagon in input().split()]
 
 left_places = False
-for i in range(len(wagon_state)):
+for i in range(len(wagons)):
 
-  if wagon_state[i] < 4:
-    people_to_add = 4 - wagon_state[i]
-    if people_to_add > people_count:
-      people_to_add = people_count
-      left_places = True
-          
-    wagon_state[i] += people_to_add
-    people_count -= people_to_add
+    if wagons[i] < MAX_SIZE:
+        free_places = MAX_SIZE - wagons[i]
+        if free_places > people_count:
+            free_places = people_count
+            left_places = True
+
+        wagons[i] += free_places
+        people_count -= free_places
 
 if left_places:
-  print("The lift has empty spots!")
-
-if people_count > 0:
+    print("The lift has empty spots!")
+elif not left_places and people_count != 0:
     print(f"There isn't enough space! {people_count} people in a queue!")
 
-print(*wagon_state, sep=" ")    
+print(*wagons, sep=" ")
