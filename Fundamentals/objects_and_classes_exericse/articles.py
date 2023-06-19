56d481f1c2f5b2537641dfb61981f3a0e887a9a0
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-19 14:17:03 +0300

    Update articles.py

--- Fundamentals/objects_and_classes_exericse/articles.py
+++ Fundamentals/objects_and_classes_exericse/articles.py
@@ -15,19 +15,3 @@ class Article:
 
   def __repr__(self):
     return f"{self.title} - {self.content}: {self.author}"
-
-article = Article(
-"Highest Recorded Temperature",
-"Temperatures across Europe are unprecedented, according to scientists.",
-"Ben Turner"
-)
-article.edit(
-"Syracuse, a city on the coast of the Italian island of Sicily, registered temperatures of 48.8 degrees Celsius"
-)
-article.rename(
-"Temperature in Italy"
-)
-article.change_author(
-"B. T."
-)
-print(article)
