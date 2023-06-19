fa48171c2f572c93d052d4bfb553c373770a4ff1
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-19 13:34:19 +0300

    Update catalogue.py

--- Fundamentals/objects_and_classes_exericse/catalogue.py
+++ Fundamentals/objects_and_classes_exericse/catalogue.py
@@ -1,22 +1,19 @@
 class Catalogue:
-    def __init__(self, name):
-        self.name = name
-        self.products = []
+  def __init__(self, name):
+    self.name = name
+    self.products = []
 
-    def add_product(self, product):
-        self.products.append(product)
+  def add_product(self, product):
+    self.products.append(product)
 
-    def get_by_letter(self, first_letter):
-        first_letter_products = []
+  def get_by_letter(self, first_letter):
+    first_letter_list = [word for word in self.products if word[0] == first_letter]
+    return first_letter_list
 
-        for product in self.products:
-            if product[0] == first_letter:
-                first_letter_products.append(product)
-        return first_letter_products
+  def __repr__(self):
+    result = f"Items in the {self.name} catalogue:"
 
-    def __repr__(self):
-        self.products.sort()
-
-        result = "Items in the {0} catalogue:\n" \
-                 "{1}".format(self.name, '\n'.join(self.products))
-        return result
+    for product in sorted(self.products):
+      result += "\n" + product
+    return result
+      
