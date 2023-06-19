83c7c7df2eb70540421f0df3f01daf9553575ff2
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-19 12:00:12 +0300

    Update email.py

--- Fundamentals/objects_and_classes_lab/email.py
+++ Fundamentals/objects_and_classes_lab/email.py
@@ -10,27 +10,23 @@ class Email:
 
   def get_info(self):
     return f"{self.sender} says to {self.receiver}: {self.content}. Sent: {self.is_sent}"
-    
+
 message = input()
 
 emails = []
 while message != "Stop":
-  message = message.split()
-  message_sender = message[0]
-  message_receiver = message[1]
-  message_content = message[2]
-
-  current_email = Email(message_sender, message_receiver, message_content)
-  emails.append(current_email)
+  message_sender, message_receiver, message_content = message.split()
+  
+  email = Email(message_sender, message_receiver, message_content)
+  emails.append(email)
 
   message = input()
 
-indexes = [int(num) for num in input().split(", ")]
+indexes = [int(index) for index in input().split(", ")]
 
-for i in range(len(emails)):
-  if i in indexes:
-    emails[i].is_sent = True
-
-for email in emails:
-  print(email.get_info())
+for index in range(len(emails)):
+  current_email = emails[index]
+  if index in indexes:
+    current_email.is_sent = True
+  print(current_email.get_info())
   
