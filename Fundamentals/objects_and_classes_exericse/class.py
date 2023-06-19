771838863d01527f5a4e1ce1c6d29b9e71b7e66b
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-19 13:49:39 +0300

    Update class.py

--- Fundamentals/objects_and_classes_exericse/class.py
+++ Fundamentals/objects_and_classes_exericse/class.py
@@ -1,6 +1,5 @@
 class Class:
   __students_count = 22
-  
   def __init__(self, name):
     self.name = name
     self.students = []
@@ -13,14 +12,10 @@ class Class:
       Class.__students_count -= 1
 
   def get_average_grade(self):
-    average_grade = sum(self.grades) / len(self.students)
+    average_grade = sum(self.grades) / len(self.grades)
     return average_grade
 
   def __repr__(self):
-    return f"The students in {self.name}: {', '.join(self.students)}. Average grade: {self.get_average_grade():.2f}"
-
-a_class = Class("11B")
-a_class.add_student("Peter", 4.80)
-a_class.add_student("George", 6.00)
-a_class.add_student("Amy", 3.50)
-print(a_class)
+    students = ", ".join(self.students)
+    return f"The students in {self.name}: {students}. Average grade: {self.get_average_grade():.2f}"
+    
