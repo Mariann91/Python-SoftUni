f0c6d1a4244f5b433236e0b57ea142a3f3f8894b
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-06-19 11:32:43 +0300

    Update party.py

--- Fundamentals/objects_and_classes_lab/party.py
+++ Fundamentals/objects_and_classes_lab/party.py
@@ -1,1 +1,17 @@
+class Party:
+  def __init__(self):
+    self.people = []
 
+
+party = Party()
+
+name = input()
+
+while name != "End":
+  party.people.append(name)
+  name = input()
+  
+people = ", ".join(party.people)
+total_people_going = len(party.people)
+
+print(f"Going: {people}\nTotal: {total_people_going}")
