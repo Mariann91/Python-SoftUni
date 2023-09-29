2f4b0aa33b96165a7d1fb5f21d9ce7f75952cca0
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-09-29 18:02:54 +0300

    Update diagonals.py

--- Python-Advanced/Multidimensional Lists - Exercise 1/diagonals.py
+++ Python-Advanced/Multidimensional Lists - Exercise 1/diagonals.py
@@ -6,9 +6,8 @@ secondary_diagonal = []
 for row in range(square_matrix_rows):
     current_row = [int(num) for num in input().split(", ")]
     negative_index = -(row + 1)
-  
     primary_diagonal.append(current_row[row])
     secondary_diagonal.append(current_row[negative_index])
 
 print(f"Primary diagonal: {', '.join([str(num) for num in primary_diagonal])}. Sum: {sum(primary_diagonal)}")
-print(f"Secondary diagonal: {', '.join([str(num) for num in primary_diagonal])}. Sum: {sum(secondary_diagonal)}")
+print(f"Secondary diagonal: {', '.join([str(num) for num in secondary_diagonal])}. Sum: {sum(secondary_diagonal)}")
