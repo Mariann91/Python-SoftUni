1ac4ce0ec696579396d90dfe067bd1494b5532a6
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-09-29 19:09:30 +0300

    Create 2x2_squares_in_matrix.py

---
+++ Python-Advanced/Multidimensional Lists - Exercise 1/2x2_squares_in_matrix.py
@@ -0,0 +1,19 @@
+matrix_rows, matrix_cols = [int(num) for num in input().split()]
+
+matrix = [input().split() for _ in range(matrix_rows)]
+equal_cells = []
+
+for row in range(matrix_rows):
+
+    for col in range(matrix_cols):
+
+        if col + 1 < matrix_cols and row + 1 < matrix_rows:
+            first = matrix[row][col]
+            second = matrix[row][col + 1]
+            third = matrix[row + 1][col]
+            fourth = matrix[row + 1][col + 1]
+
+            if first == second and second == third and third == fourth:
+                equal_cells.append([first, second, third, fourth])
+
+print(len(equal_cells))
