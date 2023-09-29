c6dea509fa3cdda85d25ea9c520aecb55f2513d3
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-09-29 17:59:57 +0300

    Create diagonals.py

---
+++ Python-Advanced/Multidimensional Lists - Exercise 1/diagonals.py
@@ -0,0 +1,14 @@
+square_matrix_rows = int(input())
+
+primary_diagonal = []
+secondary_diagonal = []
+
+for row in range(square_matrix_rows):
+    current_row = [int(num) for num in input().split(", ")]
+    negative_index = -(row + 1)
+  
+    primary_diagonal.append(current_row[row])
+    secondary_diagonal.append(current_row[negative_index])
+
+print(f"Primary diagonal: {', '.join([str(num) for num in primary_diagonal])}. Sum: {sum(primary_diagonal)}")
+print(f"Secondary diagonal: {', '.join([str(num) for num in primary_diagonal])}. Sum: {sum(secondary_diagonal)}")
