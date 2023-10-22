ab7371910645ab0a63f90966085bc27c9c0e496a
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-10-22 20:00:07 +0300

    Update rhombus_of_stars.py

--- Python-OOP/First Steps in OOP - Lab/rhombus_of_stars.py
+++ Python-OOP/First Steps in OOP - Lab/rhombus_of_stars.py
@@ -1,3 +1,5 @@
+# 1 Solution wihout functions
+
 n = int(input())
 
 # creating first triangle
@@ -9,3 +11,41 @@ for count in range(1, n + 1):
 for count in range(n - 1, -1, -1):
     print(" " * (n - count), end="")
     print(*["*"] * count)
+
+# 2 Solution with single function
+
+def print_figure(number, triangle_number):
+    
+    for count in range(1, number + 1):
+            print(" " * (number - count), end="")
+            print(*["*"] * count)
+        
+    for count in range(n - 1, -1, -1):
+        print(" " * (n - count), end="")
+        print(*["*"] * count)
+
+
+n = int(input())
+
+print_figure(n)
+
+
+# 3 Solution with 2 seperate functions 
+
+def print_row(number, count):
+    print(" " * (number - count), end="")
+    print(*["*"] * count)
+
+
+def rhombus(number):
+
+    for num in range(1, number + 1):
+        print_row(number, num)
+
+    for num in range(number - 1, -1, -1):
+        print_row(number, num)
+
+
+n = int(input())
+
+rhombus(number=n)
