88aa5e4ca6b19f36d1da9ef05a9b1fd3b8f92089
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-11-18 12:17:12 +0200

    Create desktop_computer.py

---
+++ Python-OOP/Decorators - Exercise/Computer Store/project/desktop_computer.py
@@ -0,0 +1,24 @@
+from project.computer_types.computer import Computer
+
+
+class DesktopComputer(Computer):
+
+    @property
+    def computer_processors(self):
+
+        processors = {
+            "AMD Ryzen 7 5700G": 500,
+            "Intel Core i5-12600K": 600,
+            "Apple M1 Max": 1800,
+        }
+
+        return processors
+
+    @property
+    def max_ram(self):
+        return 128
+
+    @property
+    def computer_type(self):
+        return "desktop computer"
+      
