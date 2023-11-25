c67a70293e4d147374cec87c36299d37bed3ad65
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-11-25 12:09:31 +0200

    Update base_client.py

--- Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/clients/base_client.py
+++ Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/clients/base_client.py
@@ -1,11 +1,55 @@
-from project.clients.base_client import BaseClient
+from abc import ABC, abstractmethod
 
 
-class Student(BaseClient):
-    def __init__(self, name: str, client_id: str, income: float, interest: float = 2):
-        super().__init__(name, client_id, income, interest)
+class BaseClient(ABC):
+    def __init__(self, name: str, client_id: str, income: float, interest: float) -> None:
+        self.name = name
+        self.client_id = client_id
+        self.income = income
         self.interest = interest
+        self.loans: list = []
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, value):
+
+        if value.strip() == "":
+            raise ValueError("Client name cannot be empty!")
+
+        self.__name = value
 
     @property
+    def client_id(self):
+        return self.__client_id
+
+    @client_id.setter
+    def client_id(self, value):
+
+        if len(value) != 10:
+            raise ValueError("Client ID should be 10 symbols long!")
+
+        self.__client_id = value
+
+    @property
+    def income(self):
+        return self.__income
+
+    @income.setter
+    def income(self, value):
+
+        if value <= 0:
+            raise ValueError("Income must be greater than zero!")
+
+        self.__income = value
+
+    @property
+    @abstractmethod
     def client_interest_increase(self):
-        return 1
+        ...
+    
+    def increase_clients_interest(self):
+        self.interest += self.client_interest_increase
+        
