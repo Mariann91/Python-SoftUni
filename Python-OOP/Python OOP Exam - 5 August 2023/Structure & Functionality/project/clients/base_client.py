ba5283986fb2d9c1a357cffb86ff3680f800d0b7
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-12-02 19:01:37 +0200

    Update base_client.py

--- Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/clients/base_client.py
+++ Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/clients/base_client.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class BaseClient(ABC):
-    def __init__(self, name: str, client_id: str, income: float, interest: float) -> None:
+    def __init__(self, name: str, client_id: str, income: float, interest: float):
         self.name = name
         self.client_id = client_id
         self.income = income
@@ -27,16 +27,16 @@ class BaseClient(ABC):
 
     @client_id.setter
     def client_id(self, value):
-
+        
         if len(value) != 10:
             raise ValueError("Client ID should be 10 symbols long!")
-
+        
         self.__client_id = value
-
+    
     @property
     def income(self):
         return self.__income
-
+    
     @income.setter
     def income(self, value):
 
@@ -45,11 +45,7 @@ class BaseClient(ABC):
 
         self.__income = value
 
-    @property
     @abstractmethod
-    def client_interest_increase(self):
-        ...
-    
     def increase_clients_interest(self):
-        self.interest += self.client_interest_increase
+        ...
         
