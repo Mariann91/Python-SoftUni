0d656801cb3f8b66cf102057d89c82219f84df3c
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-12-02 19:01:56 +0200

    Update student_client.py

--- Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/clients/student_client.py
+++ Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/clients/student_client.py
@@ -2,10 +2,9 @@ from project.clients.base_client import BaseClient
 
 
 class Student(BaseClient):
-    def __init__(self, name: str, client_id: str, income: float, interest: float = 2):
-        super().__init__(name, client_id, income, interest)
-        self.interest = interest
 
-    @property
-    def client_interest_increase(self):
-        return 1
+    def __init__(self, name: str, client_id: str, income: float):
+        super().__init__(name, client_id, income, interest=2)
+
+    def increase_clients_interest(self):
+        self.interest += 1
