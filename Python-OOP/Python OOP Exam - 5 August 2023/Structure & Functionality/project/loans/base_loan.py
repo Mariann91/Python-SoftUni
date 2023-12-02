60be6da1d44bc6be8e6d71adb6f3fa470c45d7c6
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-12-02 19:02:26 +0200

    Update base_loan.py

--- Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/loans/base_loan.py
+++ Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/loans/base_loan.py
@@ -2,15 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class BaseLoan(ABC):
-    def __init__(self, interest_rate: float, amount: float) -> None:
+
+    def __init__(self, interest_rate: float, amount: float):
         self.interest_rate = interest_rate
         self.amount = amount
 
-    @property
     @abstractmethod
-    def interest_rate_added_percent(self):
+    def increase_interest_rate(self):
         ...
-
-    def increase_interest_rate(self) -> None:
-        self.interest_rate += self.interest_rate_added_percent
-      
+        
