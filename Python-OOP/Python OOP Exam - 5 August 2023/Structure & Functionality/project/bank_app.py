4e78267e96ea81e2732b9421fb3fa01f11d3d48c
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-12-02 19:04:00 +0200

    Update bank_app.py

--- Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/bank_app.py
+++ Python-OOP/Python OOP Exam - 5 August 2023/Structure & Functionality/project/bank_app.py
@@ -5,81 +5,86 @@ from project.loans.student_loan import StudentLoan
 
 
 class BankApp:
-    VALID_LOANS = {
+
+    VALID_LOAN_TYPES = {
         "StudentLoan": StudentLoan,
-        "MortgageLoan": MortgageLoan
+        "MortgageLoan": MortgageLoan,
     }
 
-    VALID_CLIENTS = {
+    VALID_CLIENT_TYPES = {
         "Student": Student,
-        "Adult": Adult
+        "Adult": Adult,
     }
 
-    def __init__(self, capacity: int):
+    def __init__(self, capacity):
         self.capacity = capacity
-        self.loans = []
-        self.clients = []
-        self.loans_count_granted_to_clients = 0
-        self.granted_sum = 0
+        self.loans: list = []
+        self.clients: list = []
+
+    @staticmethod
+    def find_client(cl_id, clients_list):
+
+        for client in clients_list:
+
+            if client.client_id == cl_id:
+                return client
+
+    @staticmethod
+    def find_loan(loan_type, loans_list):
+
+        for loan in loans_list:
+
+            if loan.__class__.__name__ == loan_type:
+
+                return loan
 
     def add_loan(self, loan_type: str):
 
-        if not BankApp.VALID_LOANS.get(loan_type):
+        if loan_type not in BankApp.VALID_LOAN_TYPES.keys():
             raise Exception("Invalid loan type!")
 
-        self.loans.append(BankApp.VALID_LOANS[loan_type]())
+        self.loans.append(BankApp.VALID_LOAN_TYPES[loan_type]())
 
         return f"{loan_type} was successfully added."
 
     def add_client(self, client_type: str, client_name: str, client_id: str, income: float):
 
-        if not BankApp.VALID_CLIENTS.get(client_type):
+        if client_type not in BankApp.VALID_CLIENT_TYPES.keys():
             raise Exception("Invalid client type!")
 
         if len(self.clients) == self.capacity:
             return "Not enough bank capacity."
 
-        self.clients.append(BankApp.VALID_CLIENTS[client_type](
-            name=client_name,
-            client_id=client_id,
-            income=income,
-        ))
+        self.clients.append(BankApp.VALID_CLIENT_TYPES[client_type](client_name, client_id, income))
 
         return f"{client_type} was successfully added."
 
     def grant_loan(self, loan_type: str, client_id: str):
 
-        client = next(filter(lambda x: x.client_id == client_id, self.clients))
-
-        invalid_client_loan_pairs = {
-            "Student": "MortgageLoan",
-            "Adult": "StudentLoan"
+        not_valid_loan_type_client = {
+            "StudentLoan": "Adult",
+            "MortgageLoan": "Student",
         }
 
-        if invalid_client_loan_pairs[client.__class__.__name__] == loan_type:
+        client = self.find_client(client_id, self.clients)
+        loan = self.find_loan(loan_type, self.loans)
+
+        if not_valid_loan_type_client[loan_type] == client.__class__.__name__:
             raise Exception("Inappropriate loan type!")
 
-        for index in range(len(self.clients)):
+        self.loans.remove(loan)
+        client.loans.append(loan)
 
-            if self.loans[index].__class__.__name__ == loan_type:
-                granted_loan = self.loans.pop(index)
-
-                self.granted_sum += granted_loan.amount
-                self.loans_count_granted_to_clients += 1
-
-                client.loans.append(granted_loan)
-
-                return f"Successfully granted {loan_type} to {client.name} with ID {client_id}."
+        return f"Successfully granted {loan_type} to {client.name} with ID {client_id}."
 
     def remove_client(self, client_id: str):
 
-        try:
-            client = next(filter(lambda x: x.client_id == client_id, self.clients))
+        client = self.find_client(client_id, self.clients)
 
-        except StopIteration:
+        if not client:
             raise Exception("No such client!")
 
-        if not client.loans:
+        if client.loans:
             raise Exception("The client has loans! Removal is impossible!")
 
         self.clients.remove(client)
@@ -92,8 +97,8 @@ class BankApp:
         for loan in self.loans:
 
             if loan.__class__.__name__ == loan_type:
-                number_of_changed_loans += 1
                 loan.increase_interest_rate()
+                number_of_changed_loans += 1
 
         return f"Successfully changed {number_of_changed_loans} loans."
 
@@ -101,25 +106,36 @@ class BankApp:
         changed_client_rates_number = 0
 
         for client in self.clients:
-
             if client.interest < min_rate:
-                changed_client_rates_number += 1
 
                 client.increase_clients_interest()
+                changed_client_rates_number += 1
 
         return f"Number of clients affected: {changed_client_rates_number}."
 
     def get_statistics(self):
 
-        avg_client_interest_rate = 0
+        total_clients_count = len(self.clients)
+        total_clients_income = 0
+        loans_count_granted_to_clients = 0
+        granted_sum = 0
+        loans_count_not_granted = len(self.loans)
+        not_granted_sum = sum(loan.amount for loan in self.loans)
+        avg_client_interest_rate = 0 if len(self.clients) == 0\
+            else sum(client.interest for client in self.clients) / len(self.clients)
 
-        if len(self.clients) > 0:
-            avg_client_interest_rate = sum(client.interest for client in self.clients) / len(self.clients)
+        for client in self.clients:
 
-        return "\n".join([
-           f"Active Clients: {len(self.clients)}",
-           f"Total Income: {sum(client.income for client in self.clients):.2f}",
-           f"Granted Loans: {self.loans_count_granted_to_clients}, Total Sum: {self.granted_sum:.2f}",
-           f"Available Loans: {len(self.loans)}, Total Sum: {sum(loan.amount for loan in self.loans):.2f}",
-           f"Average Client Interest Rate: {avg_client_interest_rate:.2f}"
-        ])
+            total_clients_income += client.income
+            loans_count_granted_to_clients += len(client.loans)
+            granted_sum += sum(loan.amount for loan in client.loans)
+
+        result = [
+            f"Active Clients: {total_clients_count}",
+            f"Total Income: {total_clients_income:.2f}",
+            f"Granted Loans: {loans_count_granted_to_clients}, Total Sum: {granted_sum:.2f}",
+            f"Available Loans: {loans_count_not_granted}, Total Sum: {not_granted_sum:.2f}",
+            f"Average Client Interest Rate: {avg_client_interest_rate:.2f}"
+        ]
+
+        return "\n".join(result)
