ed744fe0fef1642d60485cc67f1713f24db19774
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2024-04-15 14:46:39 +0300

    Update electron_distribution.py

--- Fundamentals/lists_advanced_exercise/electron_distribution.py
+++ Fundamentals/lists_advanced_exercise/electron_distribution.py
@@ -1,15 +1,19 @@
 electrons = int(input())
 
+counter = 1
 shells = []
-counter = 0
-while electrons > 0:
+
+while (electrons):
+    
+    electrons_to_add = 2 * counter ** 2
+    
+    if electrons < electrons_to_add:
+        
+        electrons_to_add = electrons
+    
+    electrons -= electrons_to_add
+    shells.append(electrons_to_add)
     counter += 1
-    electron_per_shell = 2 * counter ** 2
-
-    if electron_per_shell > electrons:
-        electron_per_shell = electrons
-
-    electrons -= electron_per_shell
-    shells.append(electron_per_shell)
 
 print(shells)
+    
